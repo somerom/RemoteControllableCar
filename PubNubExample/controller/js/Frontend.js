4a2670cb59c037beb08b61cd8b27fc8665fe53fb
ronimk <ronimk@users.noreply.github.com>
2020-12-09 15:28:05 +0200

    sensoripohja webbisivulle - osa 1

--- PubNubExample/controller/js/Frontend.js
+++ PubNubExample/controller/js/Frontend.js
@@ -1,7 +1,9 @@
 // Set sensor id button listeners:
 
 function createSIDButtonListener(buttonID, sensorID) {
-    document.getElementById(buttonID).addEventListener("click", function() {
+    let btn = document.getElementById(buttonID)
+    btn.innerHTML = sensorID;
+    btn.addEventListener("click", function() {
       activeSensor = findSensorByID(sensorID);
       document.getElementById("activesensorid").innerHTML = sensorID;});
 }
