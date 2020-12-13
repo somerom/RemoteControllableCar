330e064b9ccb27ee3ac0a4158d7ea463b5612fd7
Maija Somero <maija.somero@metropolia.fi>
2020-12-13 20:59:08 +0200

    corrections 2.

--- PubNubExample/controller/js/websocket.js
+++ PubNubExample/controller/js/websocket.js
@@ -21,3 +21,20 @@ function MqttConnect() {
     };
     mqtt.connect(options);
 }
+function OnConnect2() {
+
+    message = new Paho.MQTT.Message(giveMsg);
+    message.destinationName = giveTopic;
+    mqtt.subscribe(giveTopic);
+}
+
+function MqttConnect2() {
+    console.log("connecting to" + host + " " + port);
+    mgtt = new Paho.MQTT.Client(host, port, "clientjs");
+    var options = {
+        timeout = 3,
+        onSuccess = OnConnect2(),
+
+    };
+    mqtt.connect(options);
+}
